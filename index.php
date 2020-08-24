7b338d9164224339f2eb51d6d0213d17284a249e
yiays <yesiateyoursheep@gmail.com>
2020-08-24 23:41:25 +1200

    Initial commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Passport</title>
+	
+	<link rel="stylesheet" href="/css/style.css?v=8">
+	<link rel="stylesheet" href="https://cdn.yiays.com/normalize.css">
+</head>
+<body>
+	<div class="wrapper">
+		<h1>A passport for Yiays.com is a single sign on for all Yiays.com projects!</h1>
+		<div class="icons">
+			<img src="/img/icons/yiays.svg" alt="Logo for Yiays.com" title="Yiays.com">
+			<img src="/img/icons/blog.svg" alt="Logo for Yiays Blog" title="Yiays Blog">
+			<img src="/img/icons/meme.svg" alt="Logo for MemeDB" title="MemeDB">
+			<img src="/img/icons/merely.svg" alt="Logo for Merely Services" title="Merely Services">
+			<img src="/img/icons/pukeko.svg" alt="Logo for PukekoHost" title="PukekoHost">
+			<img src="/img/icons/kahoot.svg" alt="Logo for KahootDiscord" title="KahootDiscord">
+			<img src="/img/icons/passport.svg" alt="Logo for Passport" title="Passport">
+		</div>
+		<h2>Get your passport with Google, Discord or email today!</h2>
+	</div>
+	
+	<script src="https://cdn.yiays.com/jquery-3.5.1.min.js"></script>
+</body>
+</html>
