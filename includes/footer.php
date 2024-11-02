bf51c34a1402492e516666078907c99fe71b295a
Yiays <yiays@yiays.com>
2024-11-02 16:33:45 +1300

    Remove PHP version files

--- includes/footer.php
+++
@@ -1,9 +0,0 @@
-	</div>
-	<footer>
-		&copy; 2023 Yiays
-	</footer>
-	
-	<script src="https://cdn.yiays.com/jquery-3.5.1.min.js"></script>
-	<script src="/js/ui.js?v=12"></script>
-</body>
-</html>
--- views/account/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once('api/auth.php');
-
-$user = passport\autologin();
-if($user) $user->session->revoke();
-header('Location: /');
-die();
-?>
