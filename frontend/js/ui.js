4d595af15c5e8ede13cda041dbfe7db4461ffe1c
Yiays <yesiateyoursheep@gmail.com>
2024-11-07 14:30:52 +1300

    [web] Added support for code authentication and redirection

--- frontend/js/ui.js
+++ frontend/js/ui.js
@@ -32,8 +32,8 @@ $('form').on('submit', function(e) {
 	if($(form).find('input[type=submit]').prop('disabled'))
 		return;
 	const formData = new FormData(form);
-	$(form).find('.success,.error').remove();
-	$(form).find('input').prop('disabled', true);
+	$(form).parent().find('.success,.error').remove();
+	$(form).parent().find('input').prop('disabled', true);
 	if(form.reportValidity()) {
 		$.ajax(form.action, {
 			method: form.method,
@@ -51,12 +51,19 @@ $('form').on('submit', function(e) {
 					$(form).append(`<span class=success>${message}</span>`);
 					if(data.token) {
 						passport_storeToken(data.token);
+						passport_storeProfile(data.profile);
+						if(redirect)
+							window.location = redirect;
+					}else{
+						$(form).hide();
+						$(`#${form.id}-stage2`).show();
+						$(`#${form.id}-stage2 input`).prop('disabled', false);
 					}
 				}
 				else {
 					$(form).append(`<span class=error>${data.message}</span>`);
 					setTimeout(() => {
-						$(form).find('input').prop('disabled', false);
+						$(form).parent().find('input').filter(':visible').prop('disabled', false);
 					}, 1000);
 				}
 			}
